fbcc967b6b776bd6e9afce39da5b3fdd7eff0e3c
Daniel Moraes <moraesdeveloper@gmail.com>
2020-05-14 16:48:00 -0300

    terminada implementação da controller do PEDIDOCAB

--- control/uPedidoCabController.pas
+++ control/uPedidoCabController.pas
@@ -2,21 +2,43 @@ unit uPedidoCabController;
 
 interface
 
-uses uDmPedidoCab;
+uses
+  uDmPedidoCab, uPedidoCabModel;
 
 type
   TPedidoCabController = class
     public
       function GetCodigoNovoPedido(): Integer;
+      function Inserir(oPedidoCabModel: TPedidoCabModel; out sErro: String): Boolean;
+      function Excluir(iIDPed: Integer; out sErro: String): Boolean;
+      function Atualizar(oPedidoCabModel: TPedidoCabModel; out sErro: String): Boolean;
   end;
 
 implementation
 
 { TPedidoCabController }
 
+function TPedidoCabController.Atualizar(oPedidoCabModel: TPedidoCabModel;
+  out sErro: String): Boolean;
+begin
+  Result := DmPedidoCab.Atualizar(oPedidoCabModel, sErro);
+end;
+
+function TPedidoCabController.Excluir(iIDPed: Integer;
+  out sErro: String): Boolean;
+begin
+  Result := DmPedidoCab.Excluir(iIDPed, sErro);
+end;
+
 function TPedidoCabController.GetCodigoNovoPedido: Integer;
 begin
   Result := DmPedidoCab.GetCodigoNovoPedido();
+end;
+
+function TPedidoCabController.Inserir(oPedidoCabModel: TPedidoCabModel;
+  out sErro: String): Boolean;
+begin
+  Result := DmPedidoCab.Inserir(oPedidoCabModel, sErro);
 end;
 
 end.
