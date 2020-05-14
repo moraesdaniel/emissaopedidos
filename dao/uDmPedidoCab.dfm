df855d909df961a574789520a5ab9ab8ed0ce509
Daniel Moraes <moraesdeveloper@gmail.com>
2020-05-14 15:43:39 -0300

    - Inserção dos datasets para manipulação do cabeçalho dos pedidos - Funções básicas de inserção, atualização e exclusão do cabeçalho dos pedidos

--- dao/uDmPedidoCab.dfm
+++ dao/uDmPedidoCab.dfm
@@ -2,19 +2,77 @@ object DmPedidoCab: TDmPedidoCab
   OldCreateOrder = False
   Height = 207
   Width = 416
-  object ClientDataSet1: TClientDataSet
-    Aggregates = <>
-    Params = <>
-    Left = 192
-    Top = 88
+  object SQLInserir: TSQLDataSet
+    CommandText = 
+      'INSERT INTO PEDIDOCAB '#13#10#9'(ID_PED, DTEMISSAO, NUMERO, CLIENTE) '#13#10 +
+      'VALUES '#13#10#9'(:ID_PED, :DTEMISSAO, :NUMERO, :CLIENTE)'
+    MaxBlobSize = -1
+    Params = <
+      item
+        DataType = ftUnknown
+        Name = 'ID_PED'
+        ParamType = ptInput
+      end
+      item
+        DataType = ftUnknown
+        Name = 'DTEMISSAO'
+        ParamType = ptInput
+      end
+      item
+        DataType = ftUnknown
+        Name = 'NUMERO'
+        ParamType = ptInput
+      end
+      item
+        DataType = ftUnknown
+        Name = 'CLIENTE'
+        ParamType = ptInput
+      end>
+    SQLConnection = DmConexao.SQLConexao
+    Left = 24
+    Top = 8
   end
-  object DataSetProvider1: TDataSetProvider
-    Left = 256
-    Top = 128
+  object SQLAtualizar: TSQLDataSet
+    CommandText = 
+      'UPDATE'#13#10#9'PEDIDOCAB'#13#10'SET'#13#10#9'DTEMISSAO = :DTEMISSAO,'#13#10#9'NUMERO = :NU' +
+      'MERO,'#13#10#9'CLIENTE = :CLIENTE'#13#10'WHERE'#13#10#9'ID_PED = :ID_PED'
+    MaxBlobSize = -1
+    Params = <
+      item
+        DataType = ftDate
+        Name = 'DTEMISSAO'
+        ParamType = ptInput
+      end
+      item
+        DataType = ftInteger
+        Name = 'NUMERO'
+        ParamType = ptInput
+      end
+      item
+        DataType = ftString
+        Name = 'CLIENTE'
+        ParamType = ptInput
+      end
+      item
+        DataType = ftInteger
+        Name = 'ID_PED'
+        ParamType = ptInput
+      end>
+    SQLConnection = DmConexao.SQLConexao
+    Left = 96
+    Top = 8
   end
-  object SQLDataSet1: TSQLDataSet
-    Params = <>
+  object SQLExcluir: TSQLDataSet
+    CommandText = 'DELETE FROM PEDIDOCAB WHERE ID_PED = :ID_PED'
+    MaxBlobSize = -1
+    Params = <
+      item
+        DataType = ftInteger
+        Name = 'ID_PED'
+        ParamType = ptInput
+      end>
+    SQLConnection = DmConexao.SQLConexao
     Left = 168
-    Top = 32
+    Top = 8
   end
 end
--- dao/uDmPedidoCab.pas
+++ dao/uDmPedidoCab.pas
@@ -4,17 +4,20 @@ interface
 
 uses
   System.SysUtils, System.Classes, uDmConexao, Data.FMTBcd, Data.SqlExpr,
-  Datasnap.Provider, Data.DB, Datasnap.DBClient;
+  Datasnap.Provider, Data.DB, Datasnap.DBClient, uPedidoCabModel;
 
 type
   TDmPedidoCab = class(TDataModule)
-    ClientDataSet1: TClientDataSet;
-    DataSetProvider1: TDataSetProvider;
-    SQLDataSet1: TSQLDataSet;
+    SQLInserir: TSQLDataSet;
+    SQLAtualizar: TSQLDataSet;
+    SQLExcluir: TSQLDataSet;
   private
     { Private declarations }
   public
     function GetCodigoNovoPedido() : Integer;
+    function Inserir(oPedidoCabModel: TPedidoCabModel; out sErro: String): Boolean;
+    function Excluir(iIdPed: Integer; out sErro: String): Boolean;
+    function Atualizar(oPedidoCabModel: TPedidoCabModel; out sErro: String): Boolean;
   end;
 
 var
@@ -27,6 +30,60 @@ implementation
 {$R *.dfm}
 
 { TDmPedidoCab }
+function TDmPedidoCab.Inserir(oPedidoCabModel: TPedidoCabModel; out sErro: String): Boolean;
+begin
+  with SQLInserir do begin
+    ParamByName('ID_PED').AsInteger := oPedidoCabModel.IDPed;
+    ParamByName('NUMERO').AsInteger := oPedidoCabModel.Numero;
+    ParamByName('DTEMISSAO').AsDate := oPedidoCabModel.DtEmissao;
+    ParamByName('CLIENTE').AsString := oPedidoCabModel.Cliente;
+    try
+      ExecSQL();
+      Result := True;
+    except on E: Exception do
+      begin
+        sErro := 'Falha ao tentar salvar o pedido!' + sLineBreak + E.Message;
+        Result := False;
+      end;
+    end;
+  end;
+end;
+
+function TDmPedidoCab.Atualizar(oPedidoCabModel: TPedidoCabModel;
+  out sErro: String): Boolean;
+begin
+  with SQLAtualizar do begin
+    ParamByName('ID_PED').AsInteger := oPedidoCabModel.IDPed;
+    ParamByName('NUMERO').AsInteger := oPedidoCabModel.Numero;
+    ParamByName('DTEMISSAO').AsDate := oPedidoCabModel.DtEmissao;
+    ParamByName('CLIENTE').AsString := oPedidoCabModel.Cliente;
+    try
+      ExecSQL();
+      Result := True;
+    except on E: Exception do
+      begin
+        sErro := 'Falha ao tentar atualizar o pedido!' + sLineBreak + E.Message;
+        Result := False;
+      end;
+    end;
+  end;
+end;
+
+function TDmPedidoCab.Excluir(iIdPed: Integer; out sErro: String): Boolean;
+begin
+  with SQLExcluir do begin
+    ParamByName('ID_PED').AsInteger := iIdPed;
+    try
+      ExecSQL();
+      Result := True;
+    except on E: Exception do
+      begin
+        sErro := 'Erro ao excluir o pedido!' + sLineBreak + E.Message;
+        Result := False;
+      end;
+    end;
+  end;
+end;
 
 function TDmPedidoCab.GetCodigoNovoPedido: Integer;
 var
